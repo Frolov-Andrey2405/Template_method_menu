96fd46e7029c268383eeee244cef93a449fb822a
ртивмыли <BaklanovNM@gmail.com>
2023-03-10 18:34:13 +0200

    Створено абстрактний клас "AbstractClass"

---
+++ Template_method_menu/AbstractClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_method_menu
+{
+    internal abstract class AbstractClass
+    {
+        protected bool flag = true;
+
+        protected abstract void Init();
+        protected abstract void Idle();
+
+        protected abstract void CleanUp();
+
+        public void Run()
+        {
+            Init();
+            while (flag)
+            {
+                Idle();
+            }
+            CleanUp();
+        }
+    }
+}
